dbcd04144d0377e1bda9e780845defc31914f7a7
Mariotin <gmariotin@gmail.com>
2022-06-16 19:00:15 -0300

    atualiza

---
+++ src/Componentes/Produto.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import styled from 'styled-components';
+
+const Card = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 2px 2px 5px darkgray;
+    text-align: center;
+    border: 1px solid black;
+    margin: 0.5rem;
+    `
+const Imagem = styled.img`
+    height: 14rem;
+    width: 12rem;
+`
+
+export default class Produto extends Component {
+  render() {
+    return (
+      <Card>
+        <Imagem src={this.props.imageUrl}></Imagem>
+        <p>{this.props.name}</p>
+        <p>R${this.props.preco},00</p>
+      </Card>
+    )
+  }
+}
